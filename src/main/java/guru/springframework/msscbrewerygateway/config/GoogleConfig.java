85c3d7709b72f3b8da3e494b39a43e2b820bb45f
Marc <marc.marechal@dell.com>
2021-05-10 18:04:59 +0800

    Local Host route to Beer Service

--- src/main/java/guru/springframework/msscbrewerygateway/config/GoogleConfig.java
+++ src/main/java/guru/springframework/msscbrewerygateway/config/GoogleConfig.java
@@ -3,14 +3,12 @@ package guru.springframework.msscbrewerygateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
  * Created by marecm on 5/10/2021
  */
-@Profile("google")
-@Configuration
+//@Profile("google")
+//@Configuration
 public class GoogleConfig {
 
     @Bean
---
+++ src/main/java/guru/springframework/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by marecm on 5/10/2021
+ */
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("beer-service",
+                        r-> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                                .uri("http://localhost:8080"))
+                .build();
+    }
+}
