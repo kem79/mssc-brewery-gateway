47bb607abc350b55fe0a34c0fd0883e47281a797
Marc <marc.marechal@dell.com>
2021-05-10 17:28:24 +0800

    Spring Cloud Gateway Route Configuration

---
+++ src/main/java/guru/springframework/msscbrewerygateway/config/GoogleConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by marecm on 5/10/2021
+ */
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r->r.path("/googlesearch2")
+                        .filters(f-> f.rewritePath("/googlesearch2(?<segment>/?.*)", "/${segment}"))
+                    .uri("https://www.google.com.hk")
+                )
+                .build();
+    }
+}
