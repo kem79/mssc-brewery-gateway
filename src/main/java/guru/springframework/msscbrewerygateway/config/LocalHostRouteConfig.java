8c293e641048d5270d0e79a11b59c4c138637dfd
Marc <marc.marechal@dell.com>
2021-05-11 11:09:35 +0800

    Add Route for Order Service

--- src/main/java/guru/springframework/msscbrewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/springframework/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -17,6 +17,10 @@ public class LocalHostRouteConfig {
                 .route("beer-service",
                         r-> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                                 .uri("http://localhost:8080"))
+                .route("beer-order-service",
+                        r->r.path("/api/v1/customers/**"
+                                )
+                                .uri("http://localhost:8083"))
                 .build();
     }
 }
